746a8a8d8afaedeca9934743b577bbc6bddcf2ae
Cam Warner <cameroncwarner@gmail.com>
2018-11-27 23:56:52 -0500

    Add initial project structure

---
+++ src/CSharp.Jupyter.Kernel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.Jupyter.Kernel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

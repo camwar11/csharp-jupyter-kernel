07d69956350e68f1ce6273f160cb9db7397e2198
Cam Warner <cameroncwarner@gmail.com>
2018-12-01 20:15:21 -0500

    Add unit test project

---
+++ test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Moq;
+using Xunit;
+using CSharp.Jupyter.Kernel.Sockets.Heartbeat;
+using CSharp.Jupyter.Kernel;
+
+namespace test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
